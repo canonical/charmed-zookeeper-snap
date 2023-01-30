b078c3e85e4c78583532fbbb67e6c4ec253635ef
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2023-01-30 13:55:30 +0000

    chore: add install hook for cp jvm+config files and perm setting

--- snap/local/with_config.sh
+++ snap/local/with_config.sh
@@ -2,20 +2,9 @@
 
 set -e
 
-export CONFIG_FILE="${SNAP_COMMON}/zoo.cfg"
-if [ ! -f "${CONFIG_FILE}" ]; then
-	echo "custom configuration file ${CONFIG_FILE} does not exist."
-	echo "copying default config to ${CONFIG_FILE}"
-	cp $SNAP/conf/zoo_sample.cfg $CONFIG_FILE
-fi
-
-export PATH="${SNAP}/usr/lib/jvm/default-java/bin:${PATH}"
-export JAVA_HOME="${SNAP}/usr/lib/jvm/java-8-openjdk-amd64/jre"
+export JAVA_HOME="${SNAP_COMMON}/jvm/java-8-openjdk-amd64/jre"
 export ZOO_LOG_DIR="${SNAP_COMMON}/logs"
 export ZOOCFGDIR="${SNAP_COMMON}"
-
-cp -r "${SNAP}/conf/log4j.properties" ${SNAP_COMMON}
-
 
 "${SNAP}"/usr/bin/setpriv \
     --clear-groups \
