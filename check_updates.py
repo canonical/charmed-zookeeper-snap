978db85834eb49f0fa41aed3a21c778cf683bf61
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2023-01-30 13:03:37 +0000

    chore: use snap_daemon user

--- check_updates.py
+++ check_updates.py
@@ -1,34 +1,76 @@
 #!/usr/bin/env python3
-# Copyright 2022 Canonical Ltd.
+# Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+from typing import Optional
 import requests
 import subprocess
 import re
 from bs4 import BeautifulSoup
 
-def get_releases():
+
+class Branch:
+    def __init__(self, branch_name: str):
+        self.branch_name = branch_name
+
+    @property
+    def major_version(self) -> Optional[str]:
+        major = re.match(r"\d+.\d+", self.branch_name)
+        if major:
+            return major[0]
+
+        return None
+
+    @property
+    def minor_version(self) -> Optional[str]:
+        file = subprocess.run(
+            ["git", "show", f"{self.branch_name}:snap/snapcraft.yaml"],
+            capture_output=True,
+            text=True,
+        ).stdout
+
+        match = None
+        for line in file.splitlines():
+            if match := re.match(r"(version:) \'(\d+.\d+.\d+)'", line):
+                break
+
+        if match:
+            return match[2]
+
+        return None
+
+
+def get_branches() -> set[Branch]:
+    git_branch = subprocess.run(
+        ["git", "branch", "-r"], capture_output=True, text=True
+    ).stdout
+    git_branches = set(re.findall(r"\d+.\d+/[a-z]+", git_branch))
+
+    return {Branch(branch_name=branch) for branch in git_branches}
+
+
+def get_releases() -> set[str]:
     URL = "https://dlcdn.apache.org/zookeeper/"
     page = requests.get(URL)
 
     soup = BeautifulSoup(page.content, "html.parser")
-    releases = []
+    links = soup.find_all("a")
 
-    links = soup.find_all('a')
+    releases = set()
     for link in links:
-        short_link = link['href'][:-1]
-        if 'zookeeper-' in short_link:
-            releases.append(short_link[10:])
+        short_link = link["href"][:-1]
+        if "zookeeper-" in short_link:
+            full_version = re.sub(r"zookeeper\-", "", short_link)
+            releases.add(full_version)
+
     return releases
 
-def get_branches():
-    git_branch = subprocess.run(["git", "branch", "-r"], capture_output=True, text=True).stdout
-    return re.findall("\d.\d.\d+", git_branch)
-
-def diff_versions():
-    return [ x for x in get_releases() if x not in get_branches() ]
-    
 
 if __name__ == "__main__":
-    for version in diff_versions():
-        print(version)
+    branches = get_branches()
+    releases = get_releases()
+
+    supported_minor_versions = {branch.minor_version for branch in branches}
+    new_versions = releases - supported_minor_versions
+
+    print(new_versions)
--- snap/local/with_config.sh
+++ snap/local/with_config.sh
@@ -2,18 +2,8 @@
 
 set -e
 
-export CONFIG_FILE="${SNAP_COMMON}/zoo.cfg"
-if [ ! -f "${CONFIG_FILE}" ]; then
-	echo "custom configuration file ${CONFIG_FILE} does not exist."
-	echo "copying default config to ${CONFIG_FILE}"
-	cp $SNAP/conf/zoo_sample.cfg $CONFIG_FILE
-fi
-
-export PATH="${SNAP}/usr/lib/jvm/default-java/bin:${PATH}"
-export JAVA_HOME="${SNAP}/usr/lib/jvm/java-8-openjdk-amd64/jre"
-export ZOO_LOG_DIR="${SNAP_COMMON}/logs"
-export ZOOCFGDIR="${SNAP_COMMON}"
-
-cp -r "${SNAP}/conf/log4j.properties" ${SNAP_COMMON}
-
-"${SNAP}/${1}" --config "${SNAP_COMMON}" $2
+"${SNAP}"/usr/bin/setpriv \
+    --clear-groups \
+    --reuid snap_daemon \
+    --regid snap_daemon -- \
+    "${SNAP}/${1}" --config "${SNAP_COMMON}"/conf $2
