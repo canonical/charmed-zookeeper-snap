833de94853e9e136cfd0694f0dab216630243131
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2023-01-31 14:18:05 +0000

    chore: move env-vars to snapcraft.yaml

--- snap/local/with_config.sh
+++ snap/local/with_config.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-export JAVA_HOME="${SNAP_COMMON}/jvm/java-8-openjdk-amd64/jre"
-export ZOO_LOG_DIR="${SNAP_COMMON}/logs"
-export ZOOCFGDIR="${SNAP_COMMON}"
-
 "${SNAP}"/usr/bin/setpriv \
     --clear-groups \
     --reuid snap_daemon \
